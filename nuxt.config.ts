d841e7422188b7427432347b772b98a0744d0289
kevinbaconlofty <110416406+kevinbaconlofty@users.noreply.github.com>
2023-09-19 16:09:39 -0600

    remove trailing comma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   spaLoadingTemplate: false,
   app: {
-    pageTransition: { name: 'fade', mode: 'out-in' },
+    pageTransition: { name: 'fade', mode: 'out-in' }
   },
   typescript: {
     strict: true
